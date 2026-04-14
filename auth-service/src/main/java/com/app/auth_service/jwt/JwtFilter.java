f5f78e5c91e94ec0c8cf88fcae9d4d1e5439045c
Aditya Singh <adityasingh4082002@gmail.com>
2026-04-14 22:21:28 +0530

    [Aditya Singh] Refactor. fixed errors.

--- auth-service/src/main/java/com/app/auth_service/jwt/JwtFilter.java
+++ auth-service/src/main/java/com/app/auth_service/jwt/JwtFilter.java
@@ -41,6 +41,11 @@ public class JwtFilter extends OncePerRequestFilter {
     	
         String authHeader = request.getHeader("Authorization");
 
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String token = null;
         String username = null;
 
